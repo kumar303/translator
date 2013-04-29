76b3c285218ac4e9b2a91d33795373023bb80e96
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-04-29 18:26:01 -0500

    Fixed up localization

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,7 @@
+(function() {
+"use strict";
+
+
 var events = {
   _ns: 'local',
   _doc: $(document),
@@ -5,7 +9,12 @@ var events = {
     name = name + '.' + this._ns;
     var init = opt ? {detail: opt} : {};
     init['type'] = name;
-    this._doc.trigger(init);
+    try {
+      this._doc.trigger(init);
+    } catch (e) {
+      console.log('Triggering event', name, e);
+      throw(e);
+    }
   },
   on: function _eventsOn(name, handler) {
     name = name + '.' + this._ns;
@@ -15,8 +24,12 @@ var events = {
 
 
 function Search() {
+  var self = this;
   this.term = ko.observable("");
   this.dict = ko.observable("");
+  ko.computed(function _watchDict() {
+    events.trigger('newLangDict', {dict: self.dict()});
+  });
   this.apiKey = '460cd';  // Word reference API key.
 };
 
@@ -97,26 +110,91 @@ Results.prototype.wakeUp = function _wakeUp() {
 
 
 function Loc() {
-  this.tr = ko.observable({
-    'Go': 'Go',
-    'Enter word or phrase': 'Enter word or phrase'
+  var self = this;
+
+  this.locales = {
+    'en': {
+      'Go': 'Go',
+      'Enter word or phrase': 'Enter word or phrase'
+    },
+    'es': {
+      'Go': 'Ir',
+      'Enter word or phrase': 'Ingrese palabra o frase'
+    },
+    'pl': {
+      'Go': 'Iść',
+      'Enter word or phrase': 'Wpisz słowo lub frazę'
+    },
+    'pt': {
+      'Go': 'Ir',
+      'Enter word or phrase': 'Digite palavra ou frase'
+    },
+    'gr': {
+      'Go': 'πάω',
+      'Enter word or phrase': 'Εισάγετε λέξη ή φράση'
+    },
+    'fr': {
+      'Go': 'Allez',
+      'Enter word or phrase': 'Entrez un mot ou une phrase'
+    }
+  };
+  self._default = 'en';
+  self.activeDict = ko.observable(self._default);
+  self.dict = ko.computed(function _computedDict() {
+    return self.locales[self.activeDict()];
   });
 };
 
 Loc.prototype.wakeUp = function _wakeUp() {
+  var self = this;
+
+  events.on('newLangDict', function _onNewLangDict(evt) {
+    var dict = evt.detail.dict.substr(0, 2);  // Get the origin lang code.
+    console.log('Selected new dict', dict);
+
+    if (!self.locales[dict]) {
+      console.log('Requested locale', dict, 'does not exist; setting default');
+      dict = self._default;
+    }
+
+    if (dict != self.activeDict()) {
+      self.activeDict(dict);
+    }
+  });
+};
+
+Loc.prototype.translate = function _translate(key) {
+  var self = this;
+  var loc = self.dict()[key];
+  if (!loc) {
+    console.log('Key', key, 'has not been localized for', this.activeDict());
+    loc = this.locales[this._default][key];
+  }
+  if (!loc) {
+    console.log('Key', key, 'has not been localized at all');
+    loc = key;
+  }
+  return loc;
 };
 
 
 document.addEventListener('DOMContentLoaded', function _onLoad() {
+  var loc = new Loc();
   var viewModel = {
     search: new Search(),
     results: new Results(),
-    loc: new Loc()
+    loc: loc
   };
   for (var attr in viewModel) {
     // This is a hook for views to trigger events after everyone else
     // is listening.
     viewModel[attr].wakeUp();
   }
+
+  viewModel['_'] = loc.translate.bind(loc);  // Alias to translate
+
   ko.applyBindings(viewModel);
 });
+
+
+})();
