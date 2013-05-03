e6c68de46eefc178129b26941725fc9fe4c8f1c8
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-05-03 10:00:33 -0500

    Warn about missing gaia dir when serving

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
+var fs = require('fs');
 var express = require('express');
+
 var app = express();
 var media = __dirname + '/www';
+var gaia = process.env['GAIA'] || (__dirname + '/gaia');
+var gaiaShared = gaia + '/shared';
+// URL prefix to mimic GitHub Pages project.
+var project = '/translator';
+
+fs.exists(gaia, function(exists) {
+  if (!exists) {
+    throw new Error('Could not find gaia directory as $GAIA or local submodule. ' +
+                    'Run git submodule update --init');
+  }
+});
 
 app.configure(function() {
   app.use(express.logger({format: 'dev'}));
 });
 
+// Simulate how github pages serves the static files.
 app.get('/', function (req, res) {
-  // Simulate how github pages serves the static files.
-  res.redirect('/translator/');
+  res.redirect(project + '/');
+});
+
+// Simulate gaia's shared directory for local development.
+app.configure(function() {
+  app.use(project + '/shared', express.static(gaiaShared));
 });
 
 // Serve static files such as /www/img/*, /www/manifest.webapp, etc.
 app.configure(function() {
-  app.use('/translator', express.static(media));
+  app.use(project, express.static(media));
 });
 
 
